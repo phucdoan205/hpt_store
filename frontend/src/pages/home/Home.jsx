92fbab22f60599d81f6362738c21a60128a9a22f
Vũ Duy Thái <vuduythai69@gmail.com>
2026-02-05 15:34:14 +0700

    update

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -9,7 +9,7 @@ export default function Home() {
 
         <main className="pt-16">
         <div className="max-w-7xl mx-auto p-6">
-          <h1 className="text-4xl font-bold">HPT STORE</h1>
+          <h1 className="text-gray-600 mt-2 font-bold">HPT STORE</h1>
           <p className="text-gray-600 mt-2">
             Trang bán hàng cho khách
           </p>
