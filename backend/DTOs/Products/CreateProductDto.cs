fb90b59343d20769ceea95481b6af152113ff9ee
Đoàn Văn Phúc <phucvandoan123@gmail.com>
2026-02-26 15:38:55 +0700

    update dto createorduct

--- backend/DTOs/Products/CreateProductDto.cs
+++ backend/DTOs/Products/CreateProductDto.cs
@@ -3,6 +3,8 @@ namespace backend.DTOs.Products
     public class CreateProductDto
     {
         public required string Name { get; set; }
+        public required string Slug { get; set; }
+        public required string Description { get; set; }
         public decimal Price { get; set; }
         public int CategoryId { get; set; }
         public required string Thumbnail { get; set; }
